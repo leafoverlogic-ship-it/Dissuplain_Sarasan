1d81f3cf869f0faa6fd3b235dde4392ed04948a2
Prateek Srivastava <leafoverlogic@gmail.com>
2025-12-14 10:54:23 +0530

    14-12-2025

--- lib/screens/UserNamePwd.dart
+++ lib/screens/UserNamePwd.dart
@@ -294,7 +294,7 @@ class _UserNamePwdPageState extends State<UserNamePwdPage> {
                   const SizedBox(height: 20), // spacing below Sign In
                   const Center(
                     child: Text(
-                      'v1.3.1',
+                      'v1.3.2',  // application version
                       style: TextStyle(
                         fontSize: 12,
                         color: Colors.grey,
